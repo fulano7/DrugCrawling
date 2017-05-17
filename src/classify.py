b105685c2498b1990562bf087180ca10a02a862e
fulano7 <irvm@cin.ufpe.br>
2017-05-17 19:47:12 -0300

    mais arquivos class manualmente

--- src/classify.py
+++ src/classify.py
@@ -6,26 +6,23 @@ from sklearn.pipeline import Pipeline
 
 CLASSIFIED = "../classificados/"
 
-def classify(path):
-    count = 0
-    f_debug = open("debug", "w+")
+def predict(path):
+    counter = 0
     c_file = codecs.open("../Classifier_NH.obj", "rb")
     c = pickle.load(c_file)
     for root, dir_names, file_names in os.walk(path):
         for file_name in file_names:
-            if not os.path.isfile(path+file_name):
-                continue
-            count += 1
-            f_debug.write(path+file_name+'\n')
+            #print(path+file_name)
+            counter += 1
             #pred = []
             text = ''
             doc = codecs.open(path+file_name, mode='r', encoding='utf-8')
-            print(path+file_name)
             for line in doc:
                 text += (line)
             pred = [text]
             h = c.predict(pred)
-            print(h[0])
+            print('Making prediction, doc #', str(counter), ' of ', len(file_names))
+            print('Result: ', h[0])
             if h[0] == 'drug':
                 original_doc = codecs.open(("../html/heuristic/")+(file_name[:((file_name).rfind('.'))]), mode='r', encoding='utf-8')
                 text2 = ''
@@ -33,15 +30,13 @@ def classify(path):
                     text2 += line2
                 doc2 = codecs.open(CLASSIFIED+(file_name[:((file_name).rfind('.'))]), mode="w+", encoding='utf-8')
                 doc2.write(text2)            
-                doc2.close()
+                doc2.close()            
             doc.close()
     c_file.close()
-    f_debug.close()
-    print(str(count))
 
 if not os.path.exists(CLASSIFIED):
 	os.makedirs(CLASSIFIED)
 
-#classify('../text/heuristic/drug/')
-classify('../text/')
+predict('../text/heuristic/drug/')
+predict('../text/heuristic/other/')
 
