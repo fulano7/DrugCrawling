76ad6bd2de9bc98fed218c3f41cb7dcb049d9dba
Joao Lucas Gomes de Miranda <jlgm@ug2c34.windows.cin.ufpe.br>
2017-05-18 12:23:27 -0300

    sending final version of extractor

--- src/wrapper.py
+++ src/wrapper.py
@@ -12,7 +12,7 @@ EXT = '../extraction'
 
 def onofre_w(soup):
     obj = {}
-    obj['produto'] = soup.find_all('span', id='lblProductName')[0].text
+    obj['produto'] = soup.find('span', id='lblProductName').text
     obj['price'] = soup.find('span', class_='regular-price').text
     obj['sumario'] = soup.title.text
     obj['farmacia'] = 'Onofre'
@@ -45,6 +45,42 @@ def farma22_w(soup):
     obj['farmacia'] = 'Farma 22'
     obj['site'] = 'www.farma22.com.br'
     return obj
+    
+def farmagora_w(soup):
+    obj = {}
+    obj['produto'] = soup.find('div', class_='columns small-12 hidden-for-large-up').text
+    obj['price'] = soup.find('span', class_='preco-por').text
+    obj['sumario'] = soup.title.text
+    obj['farmacia'] = 'Farmagora'
+    obj['site'] = 'www.farmagora.com.br'
+    return obj
+    
+def drogariasp_w(soup):
+    obj = {}
+    obj['produto'] = soup.find('span', class_='glis-sku-single-item').text
+    obj['price'] = soup.find('em', class_='valor-por').text
+    obj['sumario'] = soup.title.text
+    obj['farmacia'] = 'Drogaria Sao Paulo'
+    obj['site'] = 'www.drogariasaopaulo.com.br'
+    return obj
+    
+def cristorei_w(soup):
+    obj = {}
+    obj['produto'] = soup.find('li', class_='product').text
+    obj['price'] = soup.find_all('span', class_='price')[1].text
+    obj['sumario'] = soup.title.text
+    obj['farmacia'] = 'Farmacia Cristo Rei'
+    obj['site'] = 'www.farmaciacristorei.com.br'
+    return obj
+
+def sare_w(soup):
+    obj = {}
+    obj['produto'] = soup.title.text
+    obj['price'] = soup.find('p', class_='text-success').text
+    obj['sumario'] = soup.title.text
+    obj['farmacia'] = 'Sare Drogarias Online'
+    obj['site'] = 'www.saredrogarias.com.br'
+    return obj
 
 def wrap(document, filename):
     soup = BeautifulSoup(open(document), 'html.parser')
@@ -57,15 +93,25 @@ def wrap(document, filename):
         obj = farmadelivery_w(soup)
     elif('farma22' in filename):
         obj = farma22_w(soup)
+    elif('farmagora' in filename):
+        obj = farmagora_w(soup)
+    elif('drogariasaopaulo' in filename):
+        obj = drogariasp_w(soup)
+    elif('cristorei' in filename):
+        obj = cristorei_w(soup)
+    elif('saredrogarias' in filename):
+        obj = sare_w(soup)
     return obj
 
 for filename in os.listdir(PATH):
     document = os.path.join(PATH, filename)
-    obj = wrap(document, filename)
-    if (obj != {}):
-        if ('farma22' in filename):
+    try:
+        obj = wrap(document, filename)
+        if (obj != {}):
             print(obj)
-    else:
+        else:
+            continue
+    except:
         continue
             
 
